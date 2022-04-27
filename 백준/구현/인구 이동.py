397b039a5d3aecd51176fea650eb0627fdcca34f
yuna1212 <cya3559@naver.com>
2022-04-27 12:55:01 +0900

    fix: 백준 - 구현
    - 인구 이동(성공)

--- 백준/구현/인구 이동.py
+++ 백준/구현/인구 이동.py
@@ -6,6 +6,12 @@
     다른 사람들 풀이를 봐도, 푸는 방법은 틀리지 않았다.
     탐색 과정에서 오버헤드가 발생한다는데, 지금은 못찾겠다..
     링크 https://www.acmicpc.net/board/view/86503 참조해보자
+    
+    [4월 27일 해결]
+    링크 내용보다, visited를 DFS 할 때 전역변수를 참조한게 훨씬 절약이 되었다.
+    매 탐색마다 visited를 생성하고 함수 끝나면 버리는 것이 비효율 적이기 때문
+    더군다나 BFS에서 쓰던 visited에 대한 정보는 DFS 밖에서도 똑같이 사용한다.
+    이런 부분을 고쳐줬더니 시간초과 해결
 """
 from sys import stdin
 def is_unition_possible(num1, num2):
@@ -17,13 +23,13 @@ def get_party(x, y):
     global A
     delta = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     stack = [(x, y)]
-    visited = [[False for _ in range(len(A))] for _ in range(len(A))]
+    global VISITED
     neighbor = []
     while stack:
         x, y = stack.pop()
-        if visited[x][y]:
+        if VISITED[x][y]:
             continue
-        visited[x][y] = True
+        VISITED[x][y] = True
         neighbor.append((x, y))
         for dx, dy in delta:
             nx, ny = x+dx, y+dy
@@ -42,16 +48,15 @@ day = 0
 while is_moved:
     is_moved = False
     x, y = 0, 0
-    visited = [[False for _ in range(N)] for _ in range(N)]
+    global VISITED
+    VISITED = [[False for _ in range(N)] for _ in range(N)]
     parties = []
     for x in range(0, N):
         for y in range(0, N):
-            if visited[x][y]:
+            if VISITED[x][y]:
                 continue
             party = get_party(x, y)
             if len(party) > 1:
-                for nx, ny in party:
-                    visited[nx][ny] = True
                 parties.append(party)   
     if parties:
         is_moved = True
