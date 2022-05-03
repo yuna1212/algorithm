659f21f031968c4c7e269aa820201144356df5ef
yuna1212 <cya3559@naver.com>
2022-05-04 02:46:46 +0900

    update: 2019 카카오 개발자 겨울 인턴십
    - 정규식 내 풀이 일부 수정
    - 프로그래머스 좋아요 많이 받은 풀이 추가

--- 프로그래머스/2019 카카오 개발자 겨울 인턴십/튜플.py
+++ 프로그래머스/2019 카카오 개발자 겨울 인턴십/튜플.py
@@ -32,25 +32,24 @@ def solution(s):
         answer.append(list(difference)[0])
     return answer
 import re
-def solution2(s):    
+def solution2(s):
     s = s[1:-1]
     answer = []
     sets_length_of = [set()]
     # 스트링 읽어서 배열에 저장
-    tuples = re.findall(r'\{(.*?)\}', s)
+    tuples = re.findall('\{(.*?)\}', s)
+    print(tuples)
     for t in tuples:
-        if len(t) > 1:
-            t = set(map(int, t.split(',')))
-            sets_length_of.append(t)
-        else:
-            sets_length_of.append(set([int(t)]))
+        t = re.findall('\d+.*?', t)
+        sets_length_of.append(set(map(int, t)))
     sets_length_of.sort(key = lambda x: len(x))
     # 튜플 만들기
     for i in range(1, len(sets_length_of)):
         difference = sets_length_of[i] - sets_length_of[i-1]
         answer.append(list(difference)[0])    
-    return answer
-print(solution2("{{2},{2,1},{2,1,3},{2,1,3,4}}"))
-    
-    
-    
+    return answer    
+
+from collections import Counter
+def 좋아요많이받은풀이(s):
+    s = Counter(re.findall('\d+', s))
+    return list(map(int, [k for k, v in sorted(s.items(), key=lambda x: x[1], reverse=True)]))
